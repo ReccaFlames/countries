f05cfec65bb4e22b9e4c39b7ae5a857936572925
Pawel Jozwik X <pawel.jozwik2@sabre.com>
2021-08-17 22:46:30 +0200

    add all required dependencies to start react project

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <div> This is a new react app</div>
+    </React.StrictMode>,
+    document.getElementById('root'),
+);
