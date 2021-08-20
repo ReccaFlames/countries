4243b3604f5d3c1ac1f8e045bc558a9de6b70400
Pawel Jozwik X <pawel.jozwik2@sabre.com>
2021-08-20 11:58:44 +0200

    Added React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import { Route, Switch } from 'react-router-dom';
+import Country from './pages/country/Country';
+import Home from './pages/home/Home';
 import './styles.scss';
 
 const App = () => (
-    <div className="wrapper">
-        <h1> This is a new react app</h1>
-    </div>
+    <Switch>
+        <Route path="/country" component={Country} />
+        <Route path="/" component={Home} />
+    </Switch>
 );
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Link, Router } from 'react-router-dom';
+import { history } from './utils/history';
 import App from './App';
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <Router history={history}>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/country">Country</Link>
+                </li>
+            </ul>
+            <App />
+        </Router>
     </React.StrictMode>,
     document.getElementById('root')
 );
